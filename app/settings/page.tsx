afd3615f0ed96cd088e16d4e5f3d1b9867585c8a
knsxw <letsmail.khinekhant@gmail.com>
2026-01-23 12:26:58 +0700

    Finalized readme

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import TopHeader from "@/components/top-header";
-import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { useTheme } from "next-themes";
